92e036496b5043bb08ff4169d90a5a099a38dcfe
yosuke adachi <doruji1982@gmail.com>
2018-01-10 20:43:51 +0900

    time, datetime

--- PythonApplication2/DatetimeSample.py
+++ PythonApplication2/DatetimeSample.py
@@ -8,4 +8,14 @@ print(birthday.day)
 print(birthday.isoformat())
 print(date.today())
 
+from datetime import time
 
+tm = time(11,22,33,444)
+print(tm.hour)
+print(tm.microsecond)
+print(tm.isoformat())
+
+from datetime import datetime
+
+someDay = datetime(2000, 3, 28, 11, 22, 33, 12345)
+print(someDay.isoformat())
