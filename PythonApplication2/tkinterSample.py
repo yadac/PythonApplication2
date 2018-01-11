344d1cb7b285ca0cfc848f58170a56e66b955d86
yosuke adachi <doruji1982@gmail.com>
2018-01-11 22:37:08 +0900

    tkinter

--- PythonApplication2/tkinterSample.py
+++ PythonApplication2/tkinterSample.py
@@ -1,27 +1,42 @@
 import tkinter
+import tkinter.messagebox as msg
 
 root = tkinter.Tk()
+
+# button click event
+def showAlert():
+	res = msg.askokcancel("title", edt.get())
+	if (res == True):
+		lbl["text"] = "ok"
+	else:
+		lbl["text"] = "cancel"
 
 # title bar
 root.title("MyGUIApp")
 root.geometry("300x200") # px
 
-btn = tkinter.Button(root, text="click", width="5")
-lbl = tkinter.Label(root, text="xxxxxxxxxxx", foreground="#ff0000", background="#0000ff")
+# btn = tkinter.Button(root, text="click", width="5")
 edt = tkinter.Entry(root, width="5")
-valB = tkinter.BooleanVar()
-valB.set(True)
-chk = tkinter.Checkbutton(root, text="check", variable=valB)
-valI = tkinter.IntVar()
-valI.set(0)
+btn = tkinter.Button(root, text="click", width="5", command=showAlert)
+lbl = tkinter.Label(root)
 
-rdo1 = tkinter.Radiobutton(root, text="item1", variable=valI, value = 0)
-rdo2 = tkinter.Radiobutton(root, text="item2", variable=valI, value = 1)
-rdo3 = tkinter.Radiobutton(root, text="item3", variable=valI, value = 2)
+#valB = tkinter.BooleanVar()
+#valB.set(True)
+#chk = tkinter.Checkbutton(root, text="check", variable=valB)
+#valI = tkinter.IntVar()
+#valI.set(0)
+
+#rdo1 = tkinter.Radiobutton(root, text="item1", variable=valI, value = 0)
+#rdo2 = tkinter.Radiobutton(root, text="item2", variable=valI, value = 1)
+#rdo3 = tkinter.Radiobutton(root, text="item3", variable=valI, value = 2)
 
 btn.place(x = 100, y = 100);
-lbl.grid(row = 0, column = 0)
-edt.grid(row = 1, column = 0)
-chk.grid(row = 1, column = 1)
+#lbl.grid(row = 0, column = 0)
+#edt.grid(row = 1, column = 0)
+#chk.grid(row = 1, column = 1)
+
+edt.pack(side=tkinter.LEFT)
+btn.pack(side=tkinter.LEFT)
+lbl.pack(side=tkinter.LEFT)
 
 root.mainloop()
