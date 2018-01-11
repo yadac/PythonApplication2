dad9bc185979a5c2002eede21e58bd81a88375e9
yosuke adachi <doruji1982@gmail.com>
2018-01-11 22:21:09 +0900

    tkiner widget

--- PythonApplication2/tkinterSample.py
+++ PythonApplication2/tkinterSample.py
@@ -1,4 +1,27 @@
 import tkinter
 
 root = tkinter.Tk()
+
+# title bar
+root.title("MyGUIApp")
+root.geometry("300x200") # px
+
+btn = tkinter.Button(root, text="click", width="5")
+lbl = tkinter.Label(root, text="xxxxxxxxxxx", foreground="#ff0000", background="#0000ff")
+edt = tkinter.Entry(root, width="5")
+valB = tkinter.BooleanVar()
+valB.set(True)
+chk = tkinter.Checkbutton(root, text="check", variable=valB)
+valI = tkinter.IntVar()
+valI.set(0)
+
+rdo1 = tkinter.Radiobutton(root, text="item1", variable=valI, value = 0)
+rdo2 = tkinter.Radiobutton(root, text="item2", variable=valI, value = 1)
+rdo3 = tkinter.Radiobutton(root, text="item3", variable=valI, value = 2)
+
+btn.place(x = 100, y = 100);
+lbl.grid(row = 0, column = 0)
+edt.grid(row = 1, column = 0)
+chk.grid(row = 1, column = 1)
+
 root.mainloop()
