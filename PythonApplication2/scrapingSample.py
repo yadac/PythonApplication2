6bb134236da6931d648f68c4e43dc1b13b87496d
yosuke adachi <doruji1982@gmail.com>
2018-01-12 00:16:34 +0900

    scraiping

--- PythonApplication2/scrapingSample.py
+++ PythonApplication2/scrapingSample.py
@@ -1,1 +1,11 @@
+import re
+
+with open("python-site.html", encoding = "utf-8") as f:
+	html = f.read()
+
+# retrieve href attribute
+# <a href="xxx"></a>
+for row in re.findall(r"<a.*?</a>", html, re.DOTALL):
+	url = re.search(r"<a href=\"(.*?)\">", row).group(1)
+	print(url)
 
