f1864282b3d8489c7f6ffd8e2ee88279b6c3a65f
yosuke adachi <doruji1982@gmail.com>
2018-01-13 16:15:08 +0900

    beautifulsoup4

--- PythonApplication2/scrapingSample.py
+++ PythonApplication2/scrapingSample.py
@@ -1,11 +1,28 @@
 import re
 
-with open("python-site.html", encoding = "utf-8") as f:
+with open("python-site.html", encoding="utf-8") as f:
 	html = f.read()
 
 # retrieve href attribute
 # <a href="xxx"></a>
-for row in re.findall(r"<a.*?</a>", html, re.DOTALL):
-	url = re.search(r"<a href=\"(.*?)\">", row).group(1)
-	print(url)
+try:
+	for row in re.findall(r"<a.*?</a>", html, re.DOTALL):
+		# change expression from text
+		url = re.search(r"href=[\"'](.*?)[\"']", row).group(1)
+		print(url)
+except AttributeError as e:
+	print("EXCEPTION RAISE!!!")
+	print(row)
 
+print("-------------------- beautifulsoup4")
+
+from bs4 import BeautifulSoup
+
+# default encoding of open methods is not "utf-8"? 
+# -> depend on platform (in my casse, default is "shift-jis", so i need to "utf-8" explicitly).
+with open("python-site.html", encoding="utf-8") as f:
+	html = f.read()
+soup = BeautifulSoup(html, "html.parser")
+tags = soup.find_all("a")
+for tag in tags:
+	print(tag.get("href"))
